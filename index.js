485dc191b0caf5094ee0b6770df0170b18c6820b
muliCohenGini <mulic@gini-apps.com>
2021-08-19 16:54:26 +0300

    added setInterval 60000ms

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ import {formatter} from './services/timeFormatter.service.js'
 import {emailer} from './services/mail.service.js'
 
 (async () => {
-    // setInterval(async () => {
+    setInterval(async () => {
         const zohoHours = await getZohoData();
         const clockifyHours = await getClockifyData();
         if (Math.abs(zohoHours - clockifyHours) > 5) emailer(formatter(zohoHours), formatter(clockifyHours))
-    // }, 60000)
+    }, 60000)
 })();
